32fc81d71d4613b5d71b33b89bb7806136d584fc
mvpboss1004 <mvpboss1004@126.com>
2022-08-29 08:50:21 +0800

    move conftest.py to test.py

--- test.py
+++ test.py
@@ -3,6 +3,23 @@ import jpype
 import jpype.imports
 import pytest
 
+def pytest_addoption(parser):
+    parser.addoption('--namesrv', dest='namesrv', help='RocketMQ name server')
+    parser.addoption('--topic', dest='topic', help='RocketMQ topic', default='test')
+    parser.addoption('--group', dest='group', help='RocketMQ producer/consumer group', default='test')
+
+@pytest.fixture
+def namesrv(request):
+    return request.config.getoption('--namesrv')
+
+@pytest.fixture
+def topic(request):
+    return request.config.getoption('--topic')
+
+@pytest.fixture
+def group(request):
+    return request.config.getoption('--group')
+
 if __name__ == '__main__':
     jpype.startJVM(classpath=sys.argv[1].split(','))
     ret = pytest.main(['pyrocketmq']+sys.argv[2:])
