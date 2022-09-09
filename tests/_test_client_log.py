acf077274b033915eb63a8c15497b6d07f634ba2
mvpboss1004 <mvpboss1004@126.com>
2022-09-09 09:44:02 +0800

    log finally ready

--- tests/_test_client_log.py
+++ tests/_test_client_log.py
@@ -1,24 +1,16 @@
 import os
-from tempfile import NamedTemporaryFile
-
-with NamedTemporaryFile() as f:
-    root = os.path.dirname(f.name)
-    fileName = os.path.split(f.name)[-1]
-os.environ['ROCKETMQ_LOG_USESLF4J'] = 'TRUE'
-os.environ['ROCKETMQ_LOG_FILEMAXINDEX'] = '1'
-os.environ['ROCKETMQ_LOG_FILEMAXSIZE'] = '1024'
-os.environ['ROCKETMQ_LOG_LEVEL'] = 'WARN'
-
-from pyrocketmq.client.log import basicConfig, LogLevel
-args = basicConfig(root=root, fileName=fileName)
+from xml.etree.cElementTree import ElementTree
 
 import jpype
 import jpype.imports
-if not jpype.isJVMStarted():
-    jpype.startJVM(*args, classpath=os.environ.get('CLASSPATH','').split(','))
+conf = 'logback.xml'
+args = [
+    '-Dlogback.configurationFile=' + conf,
+    '-Drocketmq.client.logUseSlf4j=TRUE'
+]
+jpype.startJVM(*args, classpath=os.environ.get('CLASSPATH','').split(','))
 
-from pyrocketmq.client.log.log import getLogger
-from pyrocketmq.common.common import Throwable
+from pyrocketmq.client.log import getLogger, LogLevel
 
 class TestLog:
     def test_enums(self):
@@ -26,18 +18,24 @@ class TestLog:
         for e in (LogLevel,):
             print(','.join([str(i) for i in e]))
     
-    def test_basicConfig(self):
+    def test_log(self):
         logger = getLogger()
-        print(logger.name)
-        logger.debug(f'__{LogLevel.DEBUG.value}__')
-        logger.info(f'__{LogLevel.INFO.value}__')
-        logger.warn(f'__{LogLevel.WARN.value}__')
-        msg = 'helloworld'
-        logger.error(f'__{LogLevel.ERROR.value}__', Throwable(msg))
-        with open(os.path.join(root, fileName)) as f:
+        for level in LogLevel:
+            getattr(logger, level.value)(f'__{level.value}__')
+        
+        tree = ElementTree(conf)
+        log_level = LogLevel.LEVEL(
+            tree.find(f"./logger[@name='{logger.name}']/level").attrib['value']
+        )
+        levels = list(LogLevel)
+        for cut in range(len(levels)):
+            if log_level == levels[cut]:
+                break
+
+        log_path = tree.find(f"./appender[@name='{logger.name}Appender']/file").text
+        with open(log_path) as f:
             text = f.read()
-        for level in (LogLevel.DEBUG, LogLevel.INFO):
+        for level in levels[:cut]:
             assert(f'__{level.value}__' not in text)
-        for level in (LogLevel.WARN, LogLevel.ERROR):
+        for level in levels[cut:]:
             assert(f'__{level.value}__' in text)
-        assert(msg in text)
